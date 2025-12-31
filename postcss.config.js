afac2969c0d617cd303df7d0b4e02221c5451568
ahmednol27 <51802760-ahmednol27@users.noreply.replit.com>
2025-12-31 05:05:12 +0000

    Restored to '83bb416623a162be64594c20ca163b74015e3ce6'
    Replit-Restored-To: 83bb416623a162be64594c20ca163b74015e3ce6

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
 export default {
   plugins: {
-    tailwindcss: {
-      // Provide proper path context for CSS imports
-      config: './tailwind.config.ts',
-    },
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
